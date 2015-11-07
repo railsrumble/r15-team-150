11dc0fea0d2134c1de92831b4ec9d90e127d5c42
Rashmi Agarwal <rashmiagar@gmail.com>
2015-11-07 23:09:57 +0530

    Generating crx file

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,33 +10,32 @@ class HomeController < ApplicationController
   end
 
   def get_files
-    # if current_user.chrome_app_session_id.blank?
-    #   session_id = current_user.create_chrome_app_session_id
+    if current_user.chrome_app_session_id.blank?
+      session_id = current_user.create_chrome_app_session_id
       # copy template chrome app directory named session_id
       # add session id to SimpleEditor.js
       # create crx
       dir = File.dirname("#{Rails.root}/tmp/"+current_user.chrome_app_session_id)
-      p dir
-      p current_user.chrome_app_session_id
-      p "hello"
       FileUtils.mkdir_p(dir+"/"+current_user.chrome_app_session_id)
       FileUtils.cp_r("#{Rails.root}/public/movie-architect/.",dir+"/"+current_user.chrome_app_session_id)
-      newfile = File.open("tmp/#{current_user.chrome_app_session_id}/test.js","w")
+      newfile = File.open("#{Rails.root}/tmp/#{current_user.chrome_app_session_id}/test.js","w")
       newfile.puts "var session_id = " + current_user.chrome_app_session_id + ";"
-      oldfile = File.open('tmp/#{current_user.chrome_app_session_id}/SimpleEditor.js', "r+")
+      oldfile = File.open("#{Rails.root}/tmp/#{current_user.chrome_app_session_id}/SimpleEditor.js", "r+")
       oldfile.each_line { |line| newfile.puts line}
       oldfile.close()
       newfile.close()
-      File.delete("tmp/#{current_user.chrome_app_session_id}/SimpleEditor.js")
-      File.rename("tmp/#{current_user.chrome_app_session_id}/test.js", "tmp/#{current_user.chrome_app_session_id}/SimpleEditor.js")
+      File.delete("#{Rails.root}/tmp/#{current_user.chrome_app_session_id}/SimpleEditor.js")
+      File.rename("#{Rails.root}/tmp/#{current_user.chrome_app_session_id}/test.js", "#{Rails.root}/tmp/#{current_user.chrome_app_session_id}/SimpleEditor.js")
       
-      # CrxMake.make(
-      #   :ex_dir => "./src",
-      #   :pkey   => "./test.pem",
-      #   :crx_output => "./package/test.crx"
-      # )
-   # end
-    puts "Hi..............";
-  end
+      CrxMake.make(
+        :ex_dir => "#{Rails.root}/tmp/#{current_user.chrome_app_session_id}",
+        # :pkey   => "#{Rails.root}/tmp/#{current_user.chrome_app_session_id}.pem",
+        :crx_output => "#{Rails.root}/vendor/MovieArchitect.crx"
+      )
+      # FileUtils.rm_rf("#{Rails.root}/tmp/#{current_user.chrome_app_session_id}")
+      # File.delete("#{Rails.root}/tmp/#{current_user.chrome_app_session_id}.pem")
 
+      send_file("#{Rails.root}/vendor/MovieArchitect.crx")
+    end
+  end
 end
