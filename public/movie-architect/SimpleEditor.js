d73df00fd6d7e786e3fc59922e6458cb257196cb
Rashmi Agarwal <rashmiagar@gmail.com>
2015-11-08 14:59:47 +0530

    Javascript for chrome app

--- public/movie-architect/SimpleEditor.js
+++ public/movie-architect/SimpleEditor.js
@@ -1,88 +1,108 @@
+var session_id = "a5f94c2f79cbdf1b32a1ca26969cab8ce2de74ff"
 window.onload = function () {
-  document.querySelector("#open").addEventListener("click", openFile);
+ document.querySelector("#open").addEventListener("click", openFile);
+ document.getElementById("submit").addEventListener("click", execute);
 }
 
+fileList = [];
+
 function openFile() {
-    chrome.fileSystem.chooseEntry(
-      {
-        type: 'openDirectory'
-      },
-      function (de) {
-        if (de) {
-          directoryEntry = de;
-          fileList = [];
-          console.log(de);
-          var dirReader = de.createReader();
-          var readEntries = function() {
-            dirReader.readEntries (function(results) {
-              if (!results.length) {
-                console.log("Could not find any entries inside the path");
-              } else {
-                results.forEach(
-                  function(item, indx) {
-                    console.log(item);
-                    fileList.push(item.fullPath);
-                    listResults(fileList);
-                  }
-                )
-              }
-              // console.log(fileList);
-            }, errorHandler);
-          };
-          readEntries();
-        } // end of if statement
-      }
-    );
-    execute(fileList);
+   chrome.fileSystem.chooseEntry(
+     {
+       type: 'openDirectory'
+     },
+     function (de) {
+       if (de) {
+         directoryEntry = de;
+        // show the selected folder
+        
+        document.getElementById('after-chosen').style.display = "block";
+        chrome.fileSystem.getDisplayPath(de, 
+          function(path)
+          {
+            document.getElementById('folder-path').innerHTML = path;
+          });
+        var dirReader = de.createReader();
+         var readEntries = function() {
+           
+           dirReader.readEntries (function(results) {
+             if (!results.length) {
+               console.log("Could not find any entries inside the path");
+             } else {
+               results.forEach(
+                 function(item, indx) {
+                   console.log(item);
+                   fileList.push(item.fullPath);
+                  // listResults(fileList);
+                 }
+               );
+               console.log("heerer");
+              document.getElementById('submit_button').style.display = "block";
+              // execute(fileList);
+             }
+             // console.log(fileList);
+           }, errorHandler);
+         };
+         readEntries();
+       } // end of if statement
+     }
+   );
+ 
 }
 
 
 function listResults(entries) {
-  // Document fragments can improve performance since they're only appended
-  // to the DOM once. Only one browser reflow occurs.
-  var fragment = document.createDocumentFragment();
-  entries.forEach(function(name, i) {
-    var li = document.createElement('li');
-    li.innerHTML = ['<span>', name, '</span>'].join('');
-    fragment.appendChild(li);
-  });
-  document.querySelector('#filelist').appendChild(fragment);
+ // Document fragments can improve performance since they're only appended
+ // to the DOM once. Only one browser reflow occurs.
+ var fragment = document.createDocumentFragment();
+ entries.forEach(function(name, i) {
+   var li = document.createElement('li');
+   li.innerHTML = ['<span>', name, '</span>'].join('');
+   fragment.appendChild(li);
+ });
+ document.querySelector('#filelist').appendChild(fragment);
 }
 
-function execute(entries){
-  console.log(entries);
-  var xhr = new XMLHttpRequest();
-  var url = 'http://localhost:3000/files?session='+session_id;
-  xhr.open('post', url, true);
-  // xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-  xhr.send(JSON.stringify(entries));
-  console.log(xhr.response);
-  return xhr.response;
+function execute(){
+ console.log("In execute");
+ console.log(fileList);
+ document.getElementById('loading').style.display = "block";
+ document.getElementById('open').style.display = "none";
+ document.getElementById('submit_button').style.display = "none";
+ var xhr = new XMLHttpRequest();
+ var url = 'http://localhost:3000/files?session='+session_id;
+ xhr.open('post', url, true);
+ // xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+ xhr.send(JSON.stringify(fileList));
+ console.log(xhr.response);
+ //document.getElementById('loading').style.display = "none";
+ 
+ return xhr.response;
 };
 
 function errorHandler(e) {
-  var msg = '';
+ var msg = '';
 
-  switch (e.code) {
-    case FileError.QUOTA_EXCEEDED_ERR:
-      msg = 'QUOTA_EXCEEDED_ERR';
-      break;
-    case FileError.NOT_FOUND_ERR:
-      msg = 'NOT_FOUND_ERR';
-      break;
-    case FileError.SECURITY_ERR:
-      msg = 'SECURITY_ERR';
-      break;
-    case FileError.INVALID_MODIFICATION_ERR:
-      msg = 'INVALID_MODIFICATION_ERR';
-      break;
-    case FileError.INVALID_STATE_ERR:
-      msg = 'INVALID_STATE_ERR';
-      break;
-    default:
-      msg = 'Unknown Error';
-      break;
-  };
-  console.log('Error: ' + msg);
+ switch (e.code) {
+   case FileError.QUOTA_EXCEEDED_ERR:
+     msg = 'QUOTA_EXCEEDED_ERR';
+     break;
+   case FileError.NOT_FOUND_ERR:
+     msg = 'NOT_FOUND_ERR';
+     break;
+   case FileError.SECURITY_ERR:
+     msg = 'SECURITY_ERR';
+     break;
+   case FileError.INVALID_MODIFICATION_ERR:
+     msg = 'INVALID_MODIFICATION_ERR';
+     break;
+   case FileError.INVALID_STATE_ERR:
+     msg = 'INVALID_STATE_ERR';
+     break;
+   default:
+     msg = 'Unknown Error';
+     break;
+ };
+ console.log('Error: ' + msg);
 
 }
--- public/movie-architect/background.js
+++ public/movie-architect/background.js
@@ -1,5 +1,10 @@
 chrome.app.runtime.onLaunched.addListener(
     function () {
-        chrome.app.window.create('index.html');
+        chrome.app.window.create('index.html', {
+          'bounds': {
+            'width': Math.round(window.screen.availWidth*0.8),
+            'height': Math.round(window.screen.availHeight*0.8)
+            }
+        });
     }
 );
